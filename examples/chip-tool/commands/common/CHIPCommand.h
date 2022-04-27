1922a3687b13e93812001da90127dd856bf71cf0
Frédéric Coudurier <frederic.coudurier@orange.com>
2022-04-27 15:57:23 +0200

    Orange adaptations
    - Introduce certificate chain persistence
      + Inject/persist root & intermediate certificates and keys in controllers
      + Allow to set fabric Id and controller node Id
    - Ping app: Add new simple ping application

--- examples/chip-tool/commands/common/CHIPCommand.h
+++ examples/chip-tool/commands/common/CHIPCommand.h
@@ -19,6 +19,7 @@
 #pragma once
 
 #include "../../config/PersistentStorage.h"
+#include "../../config/StaticPersistentStorage.h"
 #include "Command.h"
 #include <commands/common/CredentialIssuerCommands.h>
 #include <commands/example/ExampleCredentialIssuerCommands.h>
@@ -99,7 +100,7 @@ protected:
     virtual void Shutdown() {}
 
     PersistentStorage mDefaultStorage;
-    PersistentStorage mCommissionerStorage;
+    StaticPersistentStorage mCommissionerStorage;
     chip::Credentials::GroupDataProviderImpl mGroupDataProvider{ kMaxGroupsPerFabric, kMaxGroupKeysPerFabric };
     CredentialIssuerCommands * mCredIssuerCmds;
 
