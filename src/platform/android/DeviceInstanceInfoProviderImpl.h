6dec3812466165dee2adbcf66785c2175e1cc3af
willyrakotoarisoa <willy.rakotoarisoa@orange.com>
2022-07-11 08:41:43 +0200

    Take vendorId and productId args into acccount

--- src/platform/android/DeviceInstanceInfoProviderImpl.h
+++ src/platform/android/DeviceInstanceInfoProviderImpl.h
@@ -28,6 +28,7 @@ class DeviceInstanceInfoProviderImpl : public Internal::GenericDeviceInstanceInf
 public:
     CHIP_ERROR GetProductName(char * buf, size_t bufSize) override;
     CHIP_ERROR GetProductId(uint16_t & productId) override;
+    CHIP_ERROR GetVendorId(uint16_t & vendorId) override;
     CHIP_ERROR GetHardwareVersionString(char * buf, size_t bufSize) override;
 
 private:
