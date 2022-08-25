7a6f36c233d0093c389c87e7ee0b85d07fe16199
Laurent Renard <laurent.renard@orange.com>
2022-08-25 18:51:26 +0200

    Add settopbox/wifi extender application

--- src/platform/ESP32/DiagnosticDataProviderImpl.cpp
+++ src/platform/ESP32/DiagnosticDataProviderImpl.cpp
@@ -48,6 +48,10 @@ using namespace ::chip::DeviceLayer;
 using namespace ::chip::DeviceLayer::Internal;
 using namespace ::chip::app::Clusters::GeneralDiagnostics;
 
+
+int8_t rssiFake = 0;
+bool isRssiFakeMode = false;
+
 namespace {
 
 InterfaceTypeEnum GetInterfaceType(const char * if_desc)
@@ -331,7 +335,13 @@ CHIP_ERROR DiagnosticDataProviderImpl::GetWiFiRssi(int8_t & rssi)
 
     if (err == ESP_OK)
     {
-        rssi = ap_info.rssi;
+        if(isRssiFakeMode) rssi = rssiFake;
+        else
+        {
+            rssi = ap_info.rssi;
+            rssiFake = rssi;
+        }
+
     }
     return CHIP_NO_ERROR;
 }
