5b8d846784cb94aa7152a1decffdab9f758afdd0
Laurent Renard <laurent.renard@orange.com>
2022-08-25 18:51:26 +0200

    Add settopbox/wifi extender application

---
+++ examples/settopbox-app/esp32/main/callbacks.cpp
@@ -0,0 +1,20 @@
+#include <app/CommandHandler.h>
+#include <app-common/zap-generated/cluster-objects.h>
+#include <app/util/af.h>
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+using Status = Protocols::InteractionModel::Status;
+
+
+bool emberAfInformationClusterMfgSpecificPingCallback(
+    chip::app::CommandHandler * commandObj, 
+    const chip::app::ConcreteCommandPath & commandPath,
+    const chip::app::Clusters::BasicInformation::Commands::MfgSpecificPing::DecodableType & commandData)
+{
+    ChipLogDetail(Test, "Settopbox-App: Responding to command");
+    commandObj->AddStatus(commandPath, Status::Success);
+    ChipLogDetail(Test, "Settopbox-App: Response sent");
+    return true;
+}
