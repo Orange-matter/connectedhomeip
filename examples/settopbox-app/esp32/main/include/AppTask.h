7e4d7d2e98ed7abd737094859abba742587d8af0
Laurent Renard <laurent.renard@orange.com>
2022-08-25 18:51:26 +0200

    Add settopbox/wifi extender application

---
+++ examples/settopbox-app/esp32/main/include/AppTask.h
@@ -0,0 +1,79 @@
+/*
+ *
+ *    Copyright (c) 2022 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "AppEvent.h"
+#include "Button.h"
+#include "LEDWidget.h"
+#include "LedRGB.h"
+#include "freertos/FreeRTOS.h"
+#include <platform/CHIPDeviceEvent.h>
+#include <platform/CHIPDeviceLayer.h>
+
+// Application-defined error codes in the CHIP_ERROR space.
+#define APP_ERROR_EVENT_QUEUE_FAILED CHIP_APPLICATION_ERROR(0x01)
+#define APP_ERROR_CREATE_TASK_FAILED CHIP_APPLICATION_ERROR(0x02)
+#define APP_ERROR_UNHANDLED_EVENT CHIP_APPLICATION_ERROR(0x03)
+#define APP_ERROR_CREATE_TIMER_FAILED CHIP_APPLICATION_ERROR(0x04)
+#define APP_ERROR_START_TIMER_FAILED CHIP_APPLICATION_ERROR(0x05)
+#define APP_ERROR_STOP_TIMER_FAILED CHIP_APPLICATION_ERROR(0x06)
+
+extern Button AppButton;
+extern LEDWidget AppLED;
+
+class AppTask
+{
+public:
+    typedef void (*RssiStateTimerCallback)(TimerHandle_t xTimer);
+
+    CHIP_ERROR StartAppTask();
+    static void AppTaskMain(void * pvParameter);
+    void PostEvent(const AppEvent * event);
+
+    void ButtonEventHandler(const uint8_t buttonHandle, uint8_t btnAction);
+
+private:
+
+    friend AppTask & GetAppTask(void);
+    CHIP_ERROR Init();
+    void initRssiTimerCallback(RssiStateTimerCallback timer_callback);
+    void DispatchEvent(AppEvent * event);
+        
+    static void RssiActionEventHandler(AppEvent * aEvent);
+    static void PushButtonEventHandler(AppEvent * aEvent);
+    static void AppEventHandler(AppEvent * aEvent);
+   
+    static void ButtonRisingCallback();
+    static void ButtonFallingCallback();
+    static void ButtonTimerCallback(TimerHandle_t xTimer);
+    static void RssiTimerCallback(TimerHandle_t xTimer);
+    static void LedTimerCallback(TimerHandle_t xTimer);
+    static void EventHandlerPlatform(const chip::DeviceLayer::ChipDeviceEvent* event, intptr_t arg);
+    static void SetRssiColor();
+
+    static AppTask sAppTask;
+};
+
+inline AppTask & GetAppTask(void)
+{
+    return AppTask::sAppTask;
+}
